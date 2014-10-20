cc00860ad55dfd092a79fd256ee74a2e9dc54078
Roel van Uden <roel.van.uden@hotmail.com>
2014-10-20 20:26:06 +0200

    Runtime changes for new async flow

--- src/server/runtime/engine.js
+++ src/server/runtime/engine.js
@@ -4,26 +4,15 @@
 var agent = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)';
 // Initialize the cheerio module.
 var cheerio = require('cheerio');
-// Initialize the promisifier module.
-var promisifier = require('promisifier.js');
-// Initialize the request module.
-var request = promisifier(require('request'));
+var http = require('http');
+var url = require('url');
 
 // ==================================================
 // Export the request function.
 // --------------------------------------------------
 module.exports.request = function* (url, encoding) {
-    // Request the resource ...
-    var response = yield request({
-        // ... with the user agent ...
-        headers: {'User-Agent': agent},
-        // ... with the encoding ...
-        encoding: encoding || 'utf8',
-        // ... with the location ...
-        url: url
-    }).first();
-    // Return the result.
-    return response.statusCode === 200 ? response.body : undefined;
+    var response = yield request(url, encoding || 'utf8');
+    return response;
 };
 
 // ==================================================
@@ -46,3 +35,33 @@ module.exports.populate = function* (resource) {
         delete resource.location;
     }
 };
+
+/**
+ * Request data from the server.
+ * @param {string} path
+ * @param {string=} encoding
+ * @return {function(function(?string, *))}
+ */
+function request(path, encoding) {
+    return function (fn) {
+        console.log(path);
+        if (typeof encoding === 'function') {
+            fn = encoding;
+            encoding = 'utf8';
+        }
+        var obj = url.parse(path);
+        obj.headers = {'User-Agent': agent};
+        http.get(obj, function (res) {
+            var data = '';
+            res.setEncoding(encoding || 'utf8');
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+            res.on('end', function () {
+                fn(undefined, data);
+            });
+        }).on('error', function (err) {
+            fn(err);
+        });
+    };
+}
