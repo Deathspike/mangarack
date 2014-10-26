0267930fdf26698fe75c9d2865be91dff6c3f08f
Deathspike <>
2014-10-26 19:03:21 +0100

    Added batch mode processing.

---
+++ src/cli/options.js
@@ -0,0 +1,14 @@
+'use strict';
+var commander = require('commander');
+
+/**
+ * Create options.
+ * @return {!Options}
+ */
+module.exports = function () {
+    return new commander.Command().version(require('../../package').version)
+        .option('-d, --duplication', 'Disable duplication prevention')
+        .option('-e, --extension <s>', 'The file extension. (Default: cbz)')
+        .option('-c, --chapter <n>', 'The chapter filter.')
+        .option('-v, --volume <n>', 'The volume filter.');
+};
