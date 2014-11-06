259b1cf798526d4650bb96a520bde47a9feb4d54
Roel van Uden <roel.van.uden@hotmail.com>
2014-11-06 23:09:14 +0100

    Well, don't commit unfinished stuff again..

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,24 +3,6 @@
 module.exports = function (grunt) {
     // Initialize the configuration.
     grunt.initConfig({
-        traceur: {
-            options: {
-                modules: 'inline'
-            },
-            shared: {
-                cwd: 'src',
-                dest: 'dist/',
-                expand: true,
-                src: '**/*.js'
-            }
-        },
-        browserify: {
-            shared: {
-                files: {
-                    'src/test.combined.js': ['src/test.js']
-                }
-            }
-        },
         jscs: {
             main: {options: {config: true}, src: 'Gruntfile.js'},
             src: {options: {config: true}, src: 'src/**/*.js'}
