0abded712f2ae878379d0b245d75fd1aa59ad36a
Deathspike <>
2014-11-15 18:28:49 +0100

    Do not tamper with dots in series names

--- src/shared/common/affix.js
+++ src/shared/common/affix.js
@@ -7,8 +7,8 @@
  * @return {string}
  */
 module.exports = function (value, length) {
-    var withSuffix = value.indexOf('.') !== -1;
-    while (withSuffix ? value.indexOf('.') < length : value.length < length) {
+    var suffix = value.indexOf('.') !== -1;
+    while (suffix ? value.indexOf('.') < length : value.length < length) {
         value = '0' + value;
     }
     return value;
