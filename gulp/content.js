80206d3e3c5f6be052028d4fb0f2003e86c0a6b3
Deathspike <>
2014-12-27 20:38:29 +0100

    Normalize everything again.
    - Removing the GUI. Not interested right now.
    - Removing Gulp in favor or Grunt. Grunt works.
    - Normalize JSDoc declarations everywhere.

--- gulp/content.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-
-gulp.task('content', function() {
-  gulp.src(['src/*', 'src/**/content/**/*', '!src/views'])
-    .pipe(plumber())
-    .pipe(gulp.dest('build'));
-});
--- gulp/watch.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var gulp = require('gulp');
-
-gulp.task('watch', ['scripts-watch', 'default'], function() {
-  gulp.watch(['src/*', 'src/content/**/*'], ['content']);
-  gulp.watch(['src/styles/**/*.less'], ['styles']);
-});
