727d3796d4b779acbcf800f519684be52e691b7f
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-02-06 13:18:43 +0100

    Minor spelling and publishing strategy updates

--- mangarack-component-core/src/converters/toSeriesType.ts
+++ mangarack-component-core/src/converters/toSeriesType.ts
@@ -3,7 +3,7 @@ import * as mio from '../default';
 let anySeriesType: {[key: string]: mio.SeriesType} = mio.SeriesType as any;
 
 /**
- * Converts the string value to a series type, or the default series type.
+ * Converts the string value to the series type, or the default series type.
  * @param value The value.
  * @return The series type.
  */
