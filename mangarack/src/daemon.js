cdb0fe8d8809ee57b3e1bc1f1eeb9190c296a4bb
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-02-19 22:18:40 +0100

    [Daemon+Web] Mount web component into daemon

--- mangarack/src/daemon.js
+++ mangarack/src/daemon.js
@@ -1,4 +1,5 @@
 (function() {
+  process.isBundled = true;
   console.log('mangarack-daemon (' + require('../package').version + ')');
   require('source-map-support').install();
   require('mangarack-runnable-daemon/dist/app');
