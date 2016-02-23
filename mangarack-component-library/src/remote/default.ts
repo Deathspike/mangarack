57b5794f5e0f7a4ad65eab59398eb3686a82175c
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-02-23 19:02:19 +0100

    [Library] Initial draft of the remote library

--- mangarack-component-library/src/remote/default.ts
+++ mangarack-component-library/src/remote/default.ts
@@ -2,10 +2,22 @@ import * as mio from './module';
 
 /**
  * Promises the remote library.
- * @param host The host.
- * @param password The password.
+ * @param host= The host.
+ * @param password= The password.
  * @return The promise for the remote library.
  */
-export async function openRemoteLibraryAsync(host: mio.IOption<string>, password: mio.IOption<string>): Promise<mio.IOption<mio.ILibrary>> {
-  throw new Error('TODO: Not yet implemented.');
+export async function openRemoteLibraryAsync(host?: mio.IOption<string>, password?: mio.IOption<string>): Promise<mio.IOption<mio.ILibrary>> {
+  let remoteLibrary = new mio.RemoteLibrary(host, password);
+  try {
+    await remoteLibrary.versionAsync();
+    return mio.option(remoteLibrary);
+  } catch (error) {
+    if (error instanceof mio.HttpServiceError) {
+      let httpServiceError = error as mio.HttpServiceError;
+      if (httpServiceError.statusCode === 401) {
+        return mio.option<mio.ILibrary>();
+      }
+    }
+    throw error;
+  }
 }
