dd5fd9e47d679010dd84f8ea0b16b87851cd4c26
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-02-23 21:01:25 +0100

    [Library] Fix remote library authentication node

---
+++ mangarack-component-library/src/remote/helpers/convert.ts
@@ -0,0 +1,17 @@
+declare let Buffer: any;
+
+/**
+ * Converts the value to base64.
+ * @internal
+ * @param value The value.
+ * @return The base64 representation of the value.
+ */
+export function convertToBase64(value: string): string {
+  if (typeof window !== 'undefined') {
+    return window.btoa(value);
+  } else if (typeof Buffer !== 'undefined') {
+    return new Buffer(value, 'binary').toString('base64');
+  } else {
+    throw new Error('No implementation of `btoa` available.');
+  }
+}
--- mangarack-component-library/src/remote/module.ts
+++ mangarack-component-library/src/remote/module.ts
@@ -5,4 +5,5 @@ export * from '../default';
 export * from './library';
 
 // library/system/helpers
+export * from './helpers/convert';
 export * from './helpers/create';
