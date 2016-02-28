2aa17274b36ce3e55dfa2ae06ffb6d8d6a8d2c94
Deathspike <>
2016-02-28 15:29:39 +0100

    [Web] Hover feedback

--- mangarack-component-web/src/components/MenuFilterGenreComponent.tsx
+++ mangarack-component-web/src/components/MenuFilterGenreComponent.tsx
@@ -8,6 +8,7 @@ export class MenuFilterGenreComponent extends mio.StatelessComponent<{genres: {[
    * Renders the component.
    */
   public render(): JSX.Element {
+    /* TODO: Add a 'reset all' button. */
     /* TODO: Limit to the genres currently in the library. */
     return (
       <div className="menuFilterGenre">
