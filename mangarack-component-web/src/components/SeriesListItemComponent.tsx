de3feb5699352f4e98e047b39ab9fce0804044f2
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-02-28 16:57:56 +0100

    [Web] Designing the series controls

--- mangarack-component-web/src/components/SeriesListItemComponent.tsx
+++ mangarack-component-web/src/components/SeriesListItemComponent.tsx
@@ -1,20 +1,18 @@
 import * as mio from '../default';
-export type SeriesItemComponentProperties = {series: mio.ILibrarySeries};
-export type SeriesItemComponentState = {imageUrl: mio.IOption<string>};
 
 /**
- * Represents a series item component.
+ * Represents a series list item component.
  */
-export class SeriesItemComponent extends mio.StatelessComponent<{series: mio.ILibrarySeries}> {
+export class SeriesListItemComponent extends mio.StatelessComponent<{series: mio.ILibrarySeries}> {
   /**
    * Renders the component.
    */
   public render(): JSX.Element {
     let numberOfUnreadChapters = this.props.series.numberOfChapters - this.props.series.numberOfReadChapters;
     return (
-      <div className="seriesItem" key={this.props.series.id}>
+      <div className="seriesListItem" key={this.props.series.id}>
         <div className="push"></div>
-        <div className="seriesItemBody">
+        <div className="seriesListItemBody">
           {(() => {
             if (numberOfUnreadChapters > 0) {
               return <span className="numberOfUnreadChapters">{numberOfUnreadChapters}</span>;
@@ -23,7 +21,7 @@ export class SeriesItemComponent extends mio.StatelessComponent<{series: mio.ILi
             }
           })()}
           <span className="title">{this.props.series.metadata.title}</span>
-          <mio.SeriesItemPreviewComponent id={this.props.series.id} />
+          <mio.SeriesListItemPreviewComponent id={this.props.series.id} />
           <span className="provider">{this.props.series.providerName}</span>
         </div>
       </div>
