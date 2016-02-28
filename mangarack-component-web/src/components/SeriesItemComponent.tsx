e7bb22f2f6d4b89da2b4da8f1a05355daeb7af29
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-02-28 14:09:57 +0100

    [Web] Preview images with load indication

--- mangarack-component-web/src/components/SeriesItemComponent.tsx
+++ mangarack-component-web/src/components/SeriesItemComponent.tsx
@@ -5,44 +5,7 @@ export type SeriesItemComponentState = {imageUrl: mio.IOption<string>};
 /**
  * Represents a series item component.
  */
-export class SeriesItemComponent extends mio.StatefulComponent<SeriesItemComponentProperties, SeriesItemComponentState> {
-  /**
-   * Initializes a new instance of the StatefulComponent class.
-   * @param properties The properties.
-   */
-  public constructor(properties: SeriesItemComponentProperties) {
-    super(properties, {imageUrl: mio.option<string>()});
-  }
-
-  /**
-   * Occurs when the component is in the process of being mounted.
-   */
-  public componentWillMount(): void {
-    /* TODO: Implement lazy loading. */
-    /* TODO: Implement visual indication for a pending image load. */
-    super.componentWillMount();
-    this._loadImageAsync(this.props.series.id);
-  }
-
-  /**
-   * Occurs when the component is in the process of being unmounted.
-   */
-  public componentWillUnmount(): void {
-    this._clearImage();
-  }
-
-  /**
-   * Occurs when the component is receiving properties.
-   * @param newProperties The new properties.
-   */
-  public componentWillReceiveProps(newProperties: SeriesItemComponentProperties) {
-    if (this.props.series.id !== newProperties.series.id) {
-      this._clearImage();
-      this.setState({imageUrl: mio.option<string>()});
-      this._loadImageAsync(newProperties.series.id);
-    }
-  }
-
+export class SeriesItemComponent extends mio.StatelessComponent<{series: mio.ILibrarySeries}> {
   /**
    * Renders the component.
    */
@@ -56,30 +19,10 @@ export class SeriesItemComponent extends mio.StatefulComponent<SeriesItemCompone
             <span className="numberOfUnreadChapters">{numberOfUnreadChapters}</span> :
             null}
           <span className="title">{this.props.series.metadata.title}</span>
-          <img className="previewImage" src={this.state.imageUrl.hasValue ? this.state.imageUrl.value : ''} />
+          <mio.SeriesItemPreviewComponent id={this.props.series.id} />
           <span className="provider">{this.props.series.providerName}</span>
         </div>
       </div>
     );
-  }
-
-  /**
-   * Clears the image.
-   */
-  private _clearImage(): void {
-    if (this.state.imageUrl.hasValue) {
-      URL.revokeObjectURL(this.state.imageUrl.value);
-    }
-  }
-
-  /**
-   * Promises to load the image.
-   * @param seriesId The series identifier.
-   */
-  private async _loadImageAsync(seriesId: number): Promise<void> {
-    let blob = await mio.openActiveLibrary().imageAsync(seriesId);
-    if (blob.hasValue) {
-      this.setState({imageUrl: mio.option(URL.createObjectURL(blob.value))});
-    }
   }
 }
