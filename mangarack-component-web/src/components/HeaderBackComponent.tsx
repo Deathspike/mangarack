fe6b68b6113d533dc216b92fb71bb147f1b20d02
Deathspike <>
2016-02-28 12:04:35 +0100

    [Web] Back button

---
+++ mangarack-component-web/src/components/HeaderBackComponent.tsx
@@ -0,0 +1,15 @@
+import * as mio from '../default';
+
+/**
+ * Represents a header back component.
+ */
+export class HeaderBackComponent extends mio.StatelessComponent<void> {
+  /**
+   * Renders the component.
+   */
+  public render(): JSX.Element {
+    return (
+      <i className="headerBack fa fa-chevron-left" onClick={() => mio.applicationActions.back()}></i>
+    );
+  }
+}
