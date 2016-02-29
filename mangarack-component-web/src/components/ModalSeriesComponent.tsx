7a1962d5d6b46310f8cba531a2fa528ecee59d16
Deathspike <>
2016-02-29 22:55:38 +0100

    [Web] Series modal component

--- mangarack-component-web/src/components/ModalSeriesComponent.tsx
+++ mangarack-component-web/src/components/ModalSeriesComponent.tsx
@@ -3,7 +3,23 @@ import * as mio from '../default';
 /**
  * Represents a modal series component.
  */
-export class ModalSeriesComponent extends mio.StatelessComponent<void> {
+export class ModalSeriesComponent extends mio.StatefulComponent<void, {text: string}> {
+  /**
+   * Occurs when the component is in the process of being mounted.
+   */
+  public componentWillMount(): void {
+    super.componentWillMount();
+    this.componentWillReceiveProps();
+  }
+
+
+  /**
+   * Occurs when the component is receiving properties.
+   */
+  public componentWillReceiveProps() {
+    this.setState({text: ''});
+  }
+
   /**
    * Renders the component.
    */
@@ -15,10 +31,27 @@ export class ModalSeriesComponent extends mio.StatelessComponent<void> {
           <i className="fa fa-times-circle" onClick={() => mio.applicationActions.setModalType(mio.ModalType.None)} />
         </div>
         <div className="modalContainerBody">
-          <input type="text" />
-          <button className="primary">Add</button>
+          <label>Series Address:</label>
+          <input autoFocus={true} type="text" value={this.state.text} onChange={(e: mio.InputFormEvent) => this._onChange(e.target.value)} />
+          <button className="primary" disabled={!Boolean(this.state.text)} onClick={() => this._onClick()}>Add</button>
         </div>
       </div>
     );
+  }
+
+  /**
+   * Occurs when the text changes.
+   * @param text The text.
+   */
+  private _onChange(text: string): void {
+    this.setState({text: text});
+  }
+
+  /**
+   * Occurs when the button is clicked.
+   */
+  private _onClick(): void {
+    /* TODO: implement this and explain how we want addresses (add label) and not searches ;-) */
+    console.log(this.state.text);
   }
 }
