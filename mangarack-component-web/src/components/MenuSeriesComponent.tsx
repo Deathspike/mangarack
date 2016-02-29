ce571de422fe110075de052c9408daf381557213
Deathspike <>
2016-02-29 23:23:02 +0100

    [Web] Support pending series in menu again

--- mangarack-component-web/src/components/MenuSeriesComponent.tsx
+++ mangarack-component-web/src/components/MenuSeriesComponent.tsx
@@ -8,23 +8,29 @@ export class MenuSeriesComponent extends mio.StatelessComponent<{series: mio.IOp
    * Renders the component.
    */
   public render(): JSX.Element {
-    if (this.props.series.hasValue) {
-      return (
-        <div className="menuSeries">
-          <div className="header">
-            <span className="text">Series</span>
-            <span className="subtext">Your Library</span>
-          </div>
-          <div className="menuSeriesControl">
-            <i className="fa fa-plus" onClick={() => mio.applicationActions.setModalType(mio.ModalType.Series)}></i>
-            <i className="fa fa-download"></i>
-            <i className="fa fa-refresh" onClick={() => mio.applicationActions.refreshSeries()}></i>
-          </div>
-          <mio.MenuSeriesListComponent series={this.props.series} />
+    return (
+      <div className="menuSeries">
+        <div className="header">
+          <span className="text">Series</span>
+          <span className="subtext">Your Library</span>
         </div>
-      );
-    } else {
-      return null;
-    }
+        {(() => {
+          if (this.props.series.hasValue) {
+            return (
+              <div>
+                <div className="menuSeriesControl">
+                  <i className="fa fa-plus" onClick={() => mio.applicationActions.setModalType(mio.ModalType.Series)}></i>
+                  <i className="fa fa-download"></i>
+                  <i className="fa fa-refresh" onClick={() => mio.applicationActions.refreshSeries()}></i>
+                </div>
+                <mio.MenuSeriesListComponent series={this.props.series.value} />
+              </div>
+            );
+          } else {
+            return <div className="pending"><i className="fa fa-spin fa-circle-o-notch"></i></div>;
+          }
+        })()}
+      </div>
+    );
   }
 }
--- mangarack-component-web/src/components/MenuSeriesListComponent.tsx
+++ mangarack-component-web/src/components/MenuSeriesListComponent.tsx
@@ -3,27 +3,21 @@ import * as mio from '../default';
 /**
  * Represents a menu series list component.
  */
-export class MenuSeriesListComponent extends mio.StatelessComponent<{series: mio.IOption<mio.ILibrarySeries[]>}> {
+export class MenuSeriesListComponent extends mio.StatelessComponent<{series: mio.ILibrarySeries[]}> {
   /**
    * Renders the component.
    */
   public render(): JSX.Element {
-    if (this.props.series.hasValue) {
-      return (
-        <div className="menuSeriesList">
-          {(() => {
-            if (!this.props.series.hasValue) {
-              return <div className="pending"><i className="fa fa-spin fa-circle-o-notch"></i></div>;
-            } else if (!this.props.series.value.length) {
-              return <div className="none">No series available.</div>;
-            } else {
-              return <div>{this.props.series.value.map(series => <mio.MenuSeriesListItemComponent series={series} />)}</div>;
-            }
-          })()}
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return (
+      <div className="menuSeriesList">
+        {(() => {
+          if (!this.props.series.length) {
+            return <div className="none">No series available.</div>;
+          } else {
+            return <div>{this.props.series.map(series => <mio.MenuSeriesListItemComponent series={series} />)}</div>;
+          }
+        })()}
+      </div>
+    );
   }
 }
