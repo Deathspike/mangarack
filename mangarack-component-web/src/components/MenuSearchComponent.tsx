e278e913f205d66257f4e03b3e5ad940c7d63896
Deathspike <>
2016-03-02 19:46:22 +0100

    [Web] Blur search on enter (mobile friendly)

--- mangarack-component-web/src/components/MenuSearchComponent.tsx
+++ mangarack-component-web/src/components/MenuSearchComponent.tsx
@@ -11,8 +11,22 @@ export class MenuSearchComponent extends mio.StatelessComponent<{search: string}
     return (
       <div className="menuSearch">
         <div className="header">Search</div>
-        <input type="text" value={this.props.search} onChange={(e: mio.InputFormEvent) => mio.menuActions.setSearch(e.target.value)} />
+        <input type="text" value={this.props.search} onChange={(e: mio.InputFormEvent) => mio.menuActions.setSearch(e.target.value)} onKeyDown={e => this._onKeyDown(e.keyCode, e.currentTarget)} />
       </div>
     );
+  }
+
+  /**
+   * Occurs when a key is pressed down.
+   * @param keyCode The key code.
+   * @param target The target.
+   */
+  private _onKeyDown(keyCode: number, target: EventTarget): void {
+    if (keyCode === 13) {
+      let element = (target as any);
+      if (element.blur && typeof element.blur === 'function') {
+        element.blur();
+      }
+    }
   }
 }
