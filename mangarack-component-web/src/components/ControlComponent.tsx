3996fadeec16426d08956b6ca9cdd85697ca27a4
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-03-08 20:13:31 +0100

    [Web] Show volumes/chapters on chapter overview

--- mangarack-component-web/src/components/ControlComponent.tsx
+++ mangarack-component-web/src/components/ControlComponent.tsx
@@ -8,13 +8,43 @@ export class ControlComponent extends mio.StatelessComponent<void> {
    * Renders the component.
    */
   public render(): JSX.Element {
-    return (
-      <div className="control">
-        <div className="chevron-left" onClick={() => mio.applicationActions.navigateBack()}><i className="fa fa-chevron-left"></i></div>
-        <div className="plus" onClick={() => mio.modalActions.setType(mio.ModalType.Series)}><i className="fa fa-plus"></i></div>
-        <div className="download" onClick={() => mio.modalActions.setType(mio.ModalType.Download)}><i className="fa fa-download"></i></div>
-        <div className="refresh" onClick={() => mio.applicationActions.refresh()}><i className="fa fa-refresh"></i></div>
-      </div>
-    );
+    // Series:
+    //  Back
+    //  Add
+    //  Download
+    //  Refresh
+    // Chapters:
+    //  Back
+    //  Delete/Empty
+    //  Download
+    //  Refresh
+    // Pages:
+    //  Back
+    //  Delete
+    //  PageOrientation
+    //  Filters (Image Normalisation, etc)
+    //  CurrentPage/NumberOfPages -> Hovering Status
+
+    let location = mio.parseLocation();
+    if (location.seriesId.hasValue) {
+      /* TODO: Hook up the 'trash' and 'download' logic. */
+      return (
+        <div className="control">
+          <div className="chevron-left" onClick={() => mio.applicationActions.navigateBack()}><i className="fa fa-chevron-left"></i></div>
+          <div className="trash" onClick={() => mio.modalActions.setType(mio.ModalType.Series)}><i className="fa fa-trash"></i></div>
+          <div className="download" onClick={() => mio.modalActions.setType(mio.ModalType.Download)}><i className="fa fa-download"></i></div>
+          <div className="refresh" onClick={() => mio.applicationActions.refreshChapters()}><i className="fa fa-refresh"></i></div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="control">
+          <div className="chevron-left" onClick={() => mio.applicationActions.navigateBack()}><i className="fa fa-chevron-left"></i></div>
+          <div className="plus" onClick={() => mio.modalActions.setType(mio.ModalType.Series)}><i className="fa fa-plus"></i></div>
+          <div className="download" onClick={() => mio.modalActions.setType(mio.ModalType.Download)}><i className="fa fa-download"></i></div>
+          <div className="refresh" onClick={() => mio.applicationActions.refreshSeries()}><i className="fa fa-refresh"></i></div>
+        </div>
+      );
+    }
   }
 }
