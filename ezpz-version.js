55900db8603b65d2aaabe1ad988e1cbbb4dc2f9d
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-05-27 21:24:01 +0200

    Dependencies disconnect, npm definitions, helpers

--- ezpz-version.js
+++ ezpz-version.js
@@ -2,26 +2,12 @@ var fs = require('fs');
 var path = require('path');
 var version = process.argv[2];
 
-function isMangarack(key) {
-    return /^mangarack/.test(key);
+if (version) {
+  fs.readdirSync(__dirname).forEach(function(folderName) {
+      if (!/^mangarack/.test(folderName)) return;
+      var packagePath = path.join(__dirname, folderName, 'package.json');
+      var package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+      package.version = version;
+      fs.writeFileSync(packagePath, JSON.stringify(package, null, '  ') + '\n');
+  });
 }
-
-fs.readdirSync(__dirname).forEach(function(folderName) {
-    if (!isMangarack(folderName)) return;
-    var packagePath = path.join(__dirname, folderName, 'package.json');
-    var package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-    if (package.dependencies) {
-        Object.keys(package.dependencies).forEach(function(dependencyName) {
-            if (!isMangarack(dependencyName)) return;
-            package.dependencies[dependencyName] = version;
-        });
-    }
-    if (package.devDependencies) {
-      Object.keys(package.devDependencies).forEach(function(dependencyName) {
-          if (!isMangarack(dependencyName)) return;
-          package.devDependencies[dependencyName] = version;
-      });
-    }
-    package.version = version;
-    fs.writeFileSync(packagePath, JSON.stringify(package, null, '  ') + '\n');
-});
