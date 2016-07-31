a25360dc308ef44621cbf555159a85238fafea74
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-07-31 15:37:28 +0200

    Fixes for runnable CLI

--- ts.d.ts
+++ ts.d.ts
@@ -1,2 +1,3 @@
+declare let console: {log: (message: string) => void};
 declare function isFinite(value: number | undefined): value is number;
 declare function setTimeout(handler: () => void, timeout: number): number;
