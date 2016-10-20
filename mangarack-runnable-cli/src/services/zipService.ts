f10a70dcba9e89f863c7a890bd35dd3e63c3bde3
Roel van Uden <r.h.g.v.uden@outlook.com>
2016-10-20 20:38:35 +0200

    [R/Cli] Fix dependencies and types.

--- mangarack-runnable-cli/src/services/zipService.ts
+++ mangarack-runnable-cli/src/services/zipService.ts
@@ -67,6 +67,6 @@ async function createDirectoriesAsync(filePath: string): Promise<void> {
     }
   }
   for (let directoryPath of directoryPaths.reverse()) {
-    await mio.promise<void>(callback => fs.mkdir(directoryPath, error => callback()));
+    await mio.promise<void>(callback => fs.mkdir(directoryPath, () => callback()));
   }
 }
