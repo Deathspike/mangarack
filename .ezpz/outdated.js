b357f1cf3791b5e0f7fc4a411fdb55ab15ff3863
Roel van Uden <r.h.g.v.uden@outlook.com>
2017-05-17 23:48:02 +0200

    Streamlined the `ezpz` tasks.

--- .ezpz/outdated.js
+++ .ezpz/outdated.js
@@ -1,5 +1,5 @@
 var childProcess = require('child_process');
 
 module.exports = function(directoryPath, folderName) {
-  childProcess.execSync('npm install --silent', {cwd: directoryPath, stdio: [0, 1, 2]});
+  childProcess.execSync('npm outdated --depth 0', {cwd: directoryPath, stdio: [0, 1, 2]});
 };
--- .ezpz/publish.js
+++ .ezpz/publish.js
@@ -1,22 +1,11 @@
 var childProcess = require('child_process');
 var fs = require('fs');
 var path = require('path');
-var rootPath = path.join(__dirname, '..');
 
-if (path.basename(rootPath) !== 'node_modules') {
-  console.log('ERROR: The containing folder must be named \'node_modules\'.');
-  process.exit(1);
-}
-
-fs.readdirSync(rootPath)
-  .sort()
-  .filter(folderName => /^mangarack-/.test(folderName))
-  .concat('mangarack')
-  .forEach(function(folderName) {
-    var packagePath = path.join(rootPath, folderName, 'package.json');
-    var package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-    if (!package.private) {
-      var directoryPath = path.join(rootPath, folderName);
-      childProcess.execSync('npm publish', {cwd: directoryPath, stdio: [0, 1, 2]});
-    }
-  });
+module.exports = function(directoryPath, folderName) {
+  var packagePath = path.join(directoryPath, 'package.json');
+  var package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+  if (!package.private) {
+    childProcess.execSync('npm publish --silent', {cwd: directoryPath, stdio: [0, 1, 2]});
+  }
+};
