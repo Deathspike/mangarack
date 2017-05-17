fd0174c108ee23a74e9d7cce35c890608db35c24
Deathspike <>
2017-05-17 23:48:02 +0200

    Streamlined the `ezpz` tasks.

--- .ezpz/watch.js
+++ .ezpz/watch.js
@@ -1,17 +1,7 @@
 var childProcess = require('child_process');
-var fs = require('fs');
-var path = require('path');
-var rootPath = path.join(__dirname, '..');
 
-if (path.basename(rootPath) !== 'node_modules') {
-  console.log('ERROR: The containing folder must be named \'node_modules\'.');
-  process.exit(1);
-}
-
-fs.readdirSync(rootPath)
-  .sort()
-  .filter(folderName => /^mangarack-/.test(folderName))
-  .forEach(function(folderName) {
-    var directoryPath = path.join(rootPath, folderName);
+module.exports = function(directoryPath, folderName) {
+  if (folderName !== 'mangarack') {
     childProcess.spawn('node', ['node_modules/typescript/bin/tsc', '-w', '-p', '.'], {cwd: directoryPath, stdio: [0, 1, 2]});
-  });
+  }
+};
