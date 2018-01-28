9ab6991a86799735fd7e9e3bcc9d2782f0d0f2b4
Deathspike <>
2018-01-28 10:19:19 +0100

    [Cli] Clean up `execAsync` option apply.

--- src/cli/exec.ts
+++ src/cli/exec.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import shared = mio.shared;
 const packagePath = path.resolve(__dirname, '../../package.json');
 
+// TODO: Consider making `commands` an object.
 export function execAsync(argv: string[]) {
   return new Promise<void>(async (resolve, reject) => {
     let packageData = await fs.readJson(packagePath);
@@ -12,18 +13,20 @@ export function execAsync(argv: string[]) {
     commander.option('--no-headless', 'disables headless mode');
     commander.command('create <url...>')
       .description('creates series metadata')
-      .action(applyOptions.bind((urls: string[]) => mio.createAsync(urls).then(resolve, reject)));
+      .action(exec<string[]>(urls => mio.createAsync(urls).then(resolve, reject)));
     commander.command('download')
       .description('download series')
-      .action(applyOptions.bind(() => mio.downloadAsync().then(resolve, reject)));
+      .action(exec(() => mio.downloadAsync().then(resolve, reject)));
     commander.command('update <url...>')
       .description('updates series metadata')
-      .action(applyOptions.bind((urls: string[]) => mio.updateAsync(urls).then(resolve, reject)));
+      .action(exec<string[]>(urls => mio.updateAsync(urls).then(resolve, reject)));
     commander.parse(argv);
   });
 }
 
-function applyOptions(this: () => void) {
-  shared.settings.browserHeadless = (commander as any).headless;
-  this.apply(undefined, arguments);
+function exec<T>(callback: (args: T) => void) {
+  return function() {
+    shared.settings.browserHeadless = (commander as any).headless;
+    callback.apply(undefined, arguments);
+  };
 }
