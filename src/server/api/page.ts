5a20085ab7fe42bce1ae3783dad0d2e36cb22771
Deathspike <>
2018-02-03 12:36:14 +0100

    [Server] Fixed page API not serving files or closing connections due to unzip bugs

--- src/server/api/page.ts
+++ src/server/api/page.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as fs from 'fs-extra';
 import * as mime from 'mime';
 import * as mio from '../';
-import * as unzip from 'unzip';
+import * as unzipper from 'unzipper';
 import shared = mio.shared;
 
 export async function pageAsync(request: express.Request, response: express.Response) {
@@ -10,17 +10,26 @@ export async function pageAsync(request: express.Request, response: express.Resp
   let metaChapterPath = shared.path.normal(request.params.providerName, request.params.seriesName, metaChapterName);
   let metaChapterExists = await fs.pathExists(metaChapterPath);
   if (metaChapterExists) {
-    let chapterStream = fs.createReadStream(metaChapterPath)
-      .on('end', () => response.sendStatus(404))
-      .pipe(unzip.Parse())
-      .on('entry', (entry: unzip.Entry) => {
-        if (entry.path !== request.params.pageName) return entry.autodrain();
-        response.set('Content-Type', mime.getType(request.params.pageName) || 'application/octet-stream');
-        (entry as any as fs.ReadStream)
-          .on('data', chunk => response.write(chunk))
-          .on('end', () => chapterStream.end() || response.end());
-      });
+    fs.createReadStream(metaChapterPath)
+      .pipe(unzipper.Parse())
+      .on('close', () => close(response))
+      .on('entry', (entry: unzipper.Entry) => process(request, response, entry));
   } else {
     response.sendStatus(404);
+  }
+}
+
+function close(response: express.Response) {
+  if (!response.headersSent) {
+    response.sendStatus(404);
+  }
+}
+
+function process(request: express.Request, response: express.Response, entry: unzipper.Entry) {
+  if (entry.path === request.params.pageName) {
+    response.set('Content-Type', mime.getType(request.params.pageName) || 'application/octet-stream');
+    entry.pipe(response);
+  } else {
+    entry.autodrain();
   }
 }
