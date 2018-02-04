c75cff3ff4c9b8f0f4a6820cf3f989a2d3e6b7c1
Deathspike <>
2018-02-04 14:58:57 +0100

    [Client] Improve loading indicator

--- src/client/components/styles/loadingStyle.ts
+++ src/client/components/styles/loadingStyle.ts
@@ -2,6 +2,9 @@ import * as mio from '../../';
 
 export const loadingStyle = {
   icon: mio.withStyle({
+    background: 'rgba(0, 0, 0, 0.75)',
+    borderRadius: 4,
+    color: '#FFFFFF',
     left: '50%',
     position: 'absolute',
     top: '50%',
