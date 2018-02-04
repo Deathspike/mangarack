e7a14855113cf2ccc4f76237478a843f634b9414
Deathspike <>
2018-02-04 11:20:15 +0100

    [Client] Basic pinch-to-zoom implementation for the chapter view. No panning boundaries yet.

--- src/web/views/ChapterView.tsx
+++ src/web/views/ChapterView.tsx
@@ -1,3 +1,4 @@
+import * as Hammer from 'hammerjs';
 import * as React from 'react';
 import * as mobxReact from 'mobx-react';
 import * as mio from '../';
@@ -6,7 +7,7 @@ import * as mio from '../';
 export class ChapterView extends React.Component<{vm: mio.ChapterViewModel}> {
   render() {
     return (
-      <div onClick={e => this._onClick(e)} style={mio.chapterStyle.imageContainer}>
+      <div ref={divElement => this._onLoad(divElement)} onClick={e => this._onClick(e)} style={mio.chapterStyle.imageContainer}>
         <img src={this.props.vm.img} style={mio.chapterStyle.image} />
       </div>
     );
@@ -23,4 +24,42 @@ export class ChapterView extends React.Component<{vm: mio.ChapterViewModel}> {
       this.props.vm.previousPageAsync();
     }
   }
+
+  private _onLoad(divElement: HTMLDivElement | null) {
+    if (divElement) makePinch(divElement);
+  }
+}
+
+// TODO: Boundaries. Clean up.
+function makePinch(el: HTMLElement) {
+  let mc = new Hammer.Manager(el);
+  let pinch = new Hammer.Pinch();
+  let pan = new Hammer.Pan();
+
+  pinch.recognizeWith(pan);
+  mc.add([pinch, pan]);
+
+  let adjustScale = 1;
+  let adjustDeltaX = 0;
+  let adjustDeltaY = 0;
+
+  let currentScale = 0;
+  let currentDeltaX = 0;
+  let currentDeltaY = 0;
+  let panDelay = 0;
+
+  mc.on('pan pinch', ev => {
+    if (ev.type === 'pan' && panDelay >= Date.now()) return;
+    currentScale = Math.max(1, adjustScale * ev.scale);
+    currentDeltaX = adjustDeltaX + (ev.deltaX / currentScale);
+    currentDeltaY = adjustDeltaY + (ev.deltaY / currentScale);
+    el.style.transform = `scale(${currentScale}) translate(${currentDeltaX}px,${currentDeltaY}px)`;
+  });
+
+  mc.on('panend pinchend', ev => {
+      adjustScale = currentScale;
+      adjustDeltaX = currentDeltaX;
+      adjustDeltaY = currentDeltaY;
+      if (ev.type === 'pinchend') panDelay = Date.now() + 200;
+  });
 }
