a14bae399425edece9dcaefbbaaf928853bab737
Deathspike <>
2018-02-08 20:39:51 +0100

    [Cli] Fixed timer.

--- src/cli/backers/Timer.ts
+++ src/cli/backers/Timer.ts
@@ -7,9 +7,9 @@ export class Timer {
 
   toString() {
     let elapsedTime = Date.now() - this._creationTime;
-    let seconds = String((Math.floor(elapsedTime / 1000) % 60, 2)).padStart(2, '0');
-    let minutes = String((Math.floor(elapsedTime / 1000 / 60) % 60, 2)).padStart(2, '0');
-    let hours = String((Math.floor(elapsedTime / 1000 / 60 / 60), 2)).padStart(2, '0');
+    let seconds = String(Math.floor(elapsedTime / 1000) % 60).padStart(2, '0');
+    let minutes = String(Math.floor(elapsedTime / 1000 / 60) % 60).padStart(2, '0');
+    let hours = String(Math.floor(elapsedTime / 1000 / 60 / 60)).padStart(2, '0');
     return `${hours}:${minutes}:${seconds}`;
   }
 }
