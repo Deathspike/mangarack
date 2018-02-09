789a2d4b3ec80cb6bd75a129ad4f1749e9b87337
Deathspike <>
2018-02-09 22:14:09 +0100

    [Client] Bind view layers to the history stack to support PWAs

--- src/client/viewModels/LayerViewModel.ts
+++ src/client/viewModels/LayerViewModel.ts
@@ -2,16 +2,24 @@ import * as mio from '../';
 import * as mobx from 'mobx';
 
 export class LayerViewModel {
+  constructor() {
+    window.addEventListener('popstate', e => this._processHistory(e.state));
+  }
+
   @mobx.action
   close() {
-    this.items.pop();
+    history.back();
   }
 
   @mobx.action
   async openAsync(awaiter: Promise<JSX.Element>) {
     await mio.loadingViewModel.loadAsync(async () => {
       let item = await awaiter;
-      mobx.runInAction(() => this.items.push(item));
+      mobx.runInAction(() => {
+        if (this.items.length) history.pushState(this.items.length, '');
+        this.items.push(item);
+        this.historyItems = [];
+      });
     });
   }
 
@@ -25,4 +33,22 @@ export class LayerViewModel {
 
   @mobx.observable
   items = [] as JSX.Element[];
+
+  @mobx.observable
+  historyItems = [] as JSX.Element[];
+
+  @mobx.action
+  private _processHistory(length: number) {
+    if (length >= this.items.length) {
+      while (this.items.length <= length && this.historyItems.length) {
+        let lastHistoryItem = this.historyItems[this.historyItems.length - 1];
+        this.items.push(lastHistoryItem);
+        this.historyItems.pop();
+      }
+    } else {
+      let lastItem = this.items[this.items.length - 1];
+      this.historyItems.push(lastItem);
+      this.items.pop();
+    }
+  }
 }
