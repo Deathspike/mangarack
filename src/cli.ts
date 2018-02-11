9abf74e6188430bc496180515d7ee17f77566482
Deathspike <>
2018-02-11 10:34:51 +0100

    [All] Replace nodejs promises for Bluebird; better stack traces.

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,4 @@
+import * as bluebird from 'bluebird';
 import * as cli from './cli/';
 import * as path from 'path';
 export {cli}
@@ -6,6 +7,7 @@ export {cli}
   if (require.main) {
     let mainName = path.basename(require.main.filename);
     if (mainName === 'mangarack-cli' || require.main === module) {
+      global.Promise = bluebird.Promise;
       cli.execAsync(process.argv).catch(error => {
         console.error((error && error.stack) || error);
         process.exit(1);
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import * as bluebird from 'bluebird';
 import * as server from './server/';
 import * as path from 'path';
 export {server}
@@ -6,6 +7,7 @@ export {server}
   if (require.main) {
     let mainName = path.basename(require.main.filename);
     if (mainName === 'mangarack-server' || require.main === module) {
+      global.Promise = bluebird.Promise;
       server.execAsync(process.argv).catch(error => {
         console.error((error && error.stack) || error);
         process.exit(1);
