627dc427d4a83ca1cadb03698218c9b8469ac699
Deathspike <>
2018-02-12 19:21:50 +0100

    [Client] Paging to previous chapter now ends up at the end of said chapter.

--- src/client/areas/chapter/initialize.tsx
+++ src/client/areas/chapter/initialize.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import * as mio from './';
 import shared = mio.shared;
 
-export async function initializeAsync(listEntry: shared.IApiListEntry, series: shared.IApiSeries, seriesChapter: shared.IApiSeriesChapter) {
+export async function initializeAsync(listEntry: shared.IApiListEntry, series: shared.IApiSeries, seriesChapter: shared.IApiSeriesChapter, wentBack = false) {
   let controlVm = new mio.ChapterControlViewModel(listEntry, series, seriesChapter);
-  let pageVm = new mio.ChapterPageViewModel(listEntry, series, seriesChapter);
+  let pageVm = new mio.ChapterPageViewModel(listEntry, series, seriesChapter, wentBack);
   await pageVm.refreshAsync();
   return <mio.ChapterController controlVm={controlVm} pageVm={pageVm} />;
 }
